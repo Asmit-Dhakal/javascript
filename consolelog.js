709f9781ea30fd276a69953aa01b7fe0fb297e87
Asmit Dhakal <107795383+Asmit-Dhakal@users.noreply.github.com>
2023-04-27 12:00:47 +0545

    First File

---
+++ consolelog.js
@@ -0,0 +1,9 @@
+console.log(console);
+console.error("Hi Harry");
+console.assert(4 > 32);
+console.clear;
+obj = { a: 1, b: 2, c: 3 }
+console.table(obj);
+console.warn("Hello");
+console.time("a");
+console.timeEnd("a");
